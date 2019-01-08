d8dc527b00d412676ed609ddeca4fcf4e9cf4129
t-yamachi <potetosaradafive@yahoo.co.jp>
2019-01-08 20:38:56 +0900

    test

---
+++ Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application
+{
+	class Program
+	{
+		static int Main()
+		{
+			DateTime sd = new DateTime (2010, 10, 12);
+			Console.WriteLine ("Starting Date : {0}", sd);
+			DateTime ed = sd.AddDays (10);
+			Console.WriteLine ("Ending Date  : {0}", ed);
+			if (sd < ed)
+				Console.WriteLine("{0} Occurs Before {1}", sd, ed);
+			Console.Read ();
+			return 0;
+			
+
+		}
+	}
+}
+
+
+
